a7e58bfc2c6924ea3f9db7806c77524076fb7859
ludovitsiket <siket.ludovit@gmail.com>
2015-08-03 17:06:43 +0200

    pokus o progress bar

--- dicomp_01.py
+++ dicomp_01.py
@@ -7,12 +7,12 @@ from glob import glob
 # skúsiť zrýchliť vyhľadávanie ak sa dá
 #hlavný program
 def progress (): #fcia pre progress bar, toto je nanič,treba normálne čosi
-    print "."
+    print ".\t"
 print "\n\033[1;34mDICOMP\n\033[1;m"
 print "\033[1;32mPROGRAM SPRACÚVA ÚDAJE Z CELEJ DATABÁZY. OPERÁCIA TRVÁ NIEKOĽKO MINÚT, ČAKAJTE PROSÍM.\033[1;m"
 name=[]  #pole so všetkými menami
 print "\n"
-for file in glob("20*/*.dcm")+glob("*/*/*/*.dcm"):  
+for file in glob("home/ludo/dokumenty/terminal_stuffs/pajton/*/*/*.dcm")+glob("*/*/*/*.dcm"):  
     ds=dicom.read_file(file)  
     mena=ds.PatientName 
     name.append(mena)
