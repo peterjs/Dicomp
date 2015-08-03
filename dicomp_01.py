70dde6d05cbd135c037cbf0560e5d9c3fab16a9b
ludovitsiket <siket.ludovit@gmail.com>
2015-08-03 17:15:33 +0200

    pozriet sa na funkcny progress bar bez dalsich library

--- dicomp_01.py
+++ dicomp_01.py
@@ -1,13 +1,10 @@
 #!/bin/python
 # coding=utf-8
 import dicom
-#import os
+import sys #potrebná ?
 from glob import glob
 # k vyhľadávaniu spraviť progres bar lebo to fakt dlho trvá
-# skúsiť zrýchliť vyhľadávanie ak sa dá
 #hlavný program
-def progress (): #fcia pre progress bar, toto je nanič,treba normálne čosi
-    print ".\t"
 print "\n\033[1;34mDICOMP\n\033[1;m"
 print "\033[1;32mPROGRAM SPRACÚVA ÚDAJE Z CELEJ DATABÁZY. OPERÁCIA TRVÁ NIEKOĽKO MINÚT, ČAKAJTE PROSÍM.\033[1;m"
 name=[]  #pole so všetkými menami
@@ -16,7 +13,7 @@ for file in glob("home/ludo/dokumenty/terminal_stuffs/pajton/*/*/*.dcm")+glob("*
     ds=dicom.read_file(file)  
     mena=ds.PatientName 
     name.append(mena)
-#    progress ();
+    print "\r.\b"  # snaha bola ...
 riadok=5 #pocet mien v riadku; teraz dat for cyklus na uhladnejsie vypisanie;zalamovat riadky
 print "\033[1;37m",name[0:],"\033[1;m\t\t\t" #výpis všetkých mien, vymyslieť ako formátovať
 dlzka=len(name)
